0de242ff4f9d7944d19c3843c7f22154bf884246
phil-c-arissian <philipcrossland@arissian.com>
2026-03-26 15:50:32 +0000

    Update ageProof to v1.1.0-beta.3625, ageProofPresence to v1.1.0-beta.3625

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgeProof",
-            url: "https://github.com/luciditi-digital-id/age-proof-packages/releases/download/v1.1.0-beta.3619/ageProof.xcframework.zip",
-            checksum: "9d9ba3364de0fdea3583d1cab4da404ad09de2dc020e5966e51cbe75de7a4cd2"
+            url: "https://github.com/luciditi-digital-id/age-proof-packages/releases/download/v1.1.0-beta.3625/ageProof.xcframework.zip",
+            checksum: "c47efdb0bd50d3343c67048a00564008f63f071e0452199fccf670017a925513"
         )
     ]
 )
